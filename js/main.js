c332fb30e8a19491cdf04c83cf9424c146485ca0
mostafamohamady <mostafa.mohamady.2000@gmail.com>
2020-03-01 17:04:15 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   //member Categories toggle
   var windowWidth = $(window).width();
-  console.log(windowWidth);
   if (windowWidth < 768) {
     $("#members-Categories").click(function() {
       $("#members-Categories-body").slideToggle("slow");
