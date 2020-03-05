29c8efa1a642474990ab579baafa0bb5af494135
mostafamohamady <mostafa.mohamady.2000@gmail.com>
2020-03-05 04:07:41 +0200

    update

--- js/main.js
+++ js/main.js
@@ -7,5 +7,50 @@ $(document).ready(function() {
       $("#members-Categories i").toggleClass("expanded");
     });
   }
-  
+});
+
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 4,
+
+  direction: getDirection(),
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    resize: function() {
+      swiper.changeDirection(getDirection());
+    }
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1
+    },
+    "@0.75": {
+      slidesPerView: 1
+    },
+    "@1.00": {
+      slidesPerView: 2
+    },
+    "@1.50": {
+      slidesPerView: 2
+    }
+  }
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? "horizontal" : "horizontal";
+
+  return direction;
+}
+
+$(".popup-gallery").magnificPopup({
+  delegate: ".swiper-slide > a",
+  type: "image",
+  gallery: {
+    enabled: true,
+    navigateByImgClick: true,
+    preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+  }
 });
