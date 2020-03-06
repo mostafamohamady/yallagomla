4ec9d3a03abe90ea714a215a4c7300c88031a54a
mostafamohamady <mostafa.mohamady.2000@gmail.com>
2020-03-07 00:28:52 +0200

    update

--- js/main.js
+++ js/main.js
@@ -9,49 +9,49 @@ $(document).ready(function() {
   }
 });
 
-var member1 = new Swiper(".member-1", {
-  slidesPerView: 4,
+(function() {
+  var member1 = new Swiper(".member-1", {
+    slidesPerView: 4,
 
-  direction: getDirection(),
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
-  on: {
-    resize: function() {
-      member1.changeDirection(getDirection());
+    direction: getDirection(),
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    on: {
+      resize: function() {
+        member1.changeDirection(getDirection());
+      }
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1
+      },
+      "@0.75": {
+        slidesPerView: 1
+      },
+      "@1.00": {
+        slidesPerView: 2
+      },
+      "@1.50": {
+        slidesPerView: 2
+      }
     }
-  },
-  breakpoints: {
-    "@0.00": {
-      slidesPerView: 1
-    },
-    "@0.75": {
-      slidesPerView: 1
-    },
-    "@1.00": {
-      slidesPerView: 2
-    },
-    "@1.50": {
-      slidesPerView: 2
+  });
+
+  function getDirection() {
+    var direction = window.innerWidth <= 760 ? "horizontal" : "horizontal";
+
+    return direction;
+  }
+
+  $(".popup-gallery").magnificPopup({
+    delegate: ".swiper-slide > a",
+    type: "image",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
     }
-  }
-});
-
-
-function getDirection() {
- 
-  var direction = window.innerWidth <= 760 ? "horizontal" : "horizontal";
-
-  return direction;
-}
-
-$(".popup-gallery").magnificPopup({
-  delegate: ".swiper-slide > a",
-  type: "image",
-  gallery: {
-    enabled: true,
-    navigateByImgClick: true,
-    preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-  }
-});
+  });
+})();
